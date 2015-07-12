b6950939a3d722f76d16810e1b4ec29a766ffdcf
David Barragán Merino <david.barragan@kaleidos.net>
2015-07-12 19:43:47 +0200

    Create the router

---
+++ cinemair/routers.py
@@ -0,0 +1,11 @@
+from cinemair.common.api import routers
+
+
+router = routers.DefaultRouter(trailing_slash=False)
+
+from cinemair.cinemas.api import CinemasViewSet
+router.register(r"cinemas", CinemasViewSet, base_name="cinemas")
+
+from cinemair.films.api import FilmsViewSet
+router.register(r"films", FilmsViewSet, base_name="films")
+
--- cinemair/urls.py
+++ cinemair/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from .routers import router
+
 
 urlpatterns = [
     url(r"^admin/", include(admin.site.urls)),
-    url(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework"))
+    url(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+
+    url(r'^api/v1/', include(router.urls)),
 ]
