14043a783e2ebd6c4a27a38f08ca75e6e31dd5d8
David Barragán Merino <david.barragan@kaleidos.net>
2016-07-04 18:39:53 +0200

    Add show admin panel

--- cinemair/shows/admin.py
+++ cinemair/shows/admin.py
@@ -6,3 +6,16 @@ from . import models
 class ShowsInline(admin.TabularInline):
     model = models.Show
     extra = 0
+
+
+@admin.register(models.Show)
+class Show(admin.ModelAdmin):
+    fieldsets = (
+        (None, {"fields": ("cinema", "movie", "datetime")}),
+    )
+    list_display = ("id", "cinema", "movie", "datetime")
+    #list_editable = (,)
+    list_filter = ("cinema",)
+    search_fields = ("id", "cinema__name", "movie__title", "datetime")
+    date_hierarchy = "datetime"
+    ordering = ("cinema", "datetime")
