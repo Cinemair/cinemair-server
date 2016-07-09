6af31c064538c1d4d36d553ebcc2481ae06ba982
David Barragán Merino <david.barragan@kaleidos.net>
2016-07-09 21:11:58 +0200

    Fix Auth viewset

--- cinemair/users/api.py
+++ cinemair/users/api.py
@@ -3,12 +3,15 @@ from cinemair.common import exceptions as exc
 from cinemair.common import responses as res
 
 from . import services
+from . import serializers
 
 
 class AuthViewSet(viewsets.GenericViewSet):
+    serializer_class = serializers.UserSerializer
+
     # Login/Register view: /api/v1/users/auth
     def create(self, request, **kwargs):
-        login_type = request.DATA.get("type", None)
+        login_type = request.data.get("type", None)
 
         if login_type == "google":
             data = services.google_login(request)
