17ce844db6264118d614169410c25c1b024fff1f
David Barragán Merino <david.barragan@kaleidos.net>
2016-07-17 14:39:09 +0200

    Order events

--- cinemair/events/services.py
+++ cinemair/events/services.py
@@ -2,7 +2,9 @@ from .models import Event
 
 
 def get_all_events():
-    return Event.objects.select_related('user',
+    return (Event.objects.select_related('user',
                                         "show",
                                         "show__cinema",
-                                        "show__movie").all()
+                                        "show__movie")
+                        .all()
+                        .order_by("show__datetime", "show__cinema", "show__id"))
